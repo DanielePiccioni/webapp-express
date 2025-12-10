9ab2facb61ead932d9d81f25885543c62707627f
DanielePiccioni <danielepiccioni1@gmail.com>
2025-12-10 18:34:23 +0100

    add route show to take film and review film

--- routes/movies.js
+++ routes/movies.js
@@ -12,4 +12,26 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+    const movieId = req.params.id;
+    const movieSql = "SELECT * FROM movies WHERE id = ?";
+    const reviewsSql = "SELECT * FROM reviews WHERE movie_id = ?";
+
+    db.query(movieSql, [movieId], (err, movieResult) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (movieResult.length === 0)
+
+            return res.status(404).json({ error: "Film non trovato" });
+
+        db.query(reviewsSql, [movieId], (err, reviewsResult) => {
+            if (err) return res.status(500).json({ error: err.message });
+
+            res.json({
+                movie: movieResult[0],
+                reviews: reviewsResult
+            });
+        });
+    });
+});
+
 module.exports = router;
