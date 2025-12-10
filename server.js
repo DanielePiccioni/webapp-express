a85449ae8ec5fad6e9ef7dc8ba94daacc9b3692d
DanielePiccioni <danielepiccioni1@gmail.com>
2025-12-10 16:54:24 +0100

    create connection to mysql

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ const express = require("express");
 const mysql = require("mysql2");
 const app = express();
 const PORT = 3000;
+const connection = require("./database/connection");
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "movies"
-});
+app.use(express.static(`public`))
+app.use(express.json());
 
-db.connect(err => {
-    if (err) {
-        console.error("Errore connessione:", err);
-        return;
-    }
-    console.log("MySQL connesso");
-});
+const moviesRouter = require("./routes/movies");
+app.use("/movies", moviesRouter);
 
 app.listen(PORT, () =>
     console.log(`Server attivo su http://localhost:${PORT}`)
 );
+app.get(`/`, (req, res) => {
+    res.send(`Welcome to my blog`)
+})
